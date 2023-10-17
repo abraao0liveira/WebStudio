df84392cfd9545419c20e89d5fc2d54274beadbd
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-16 22:02:09 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { defaultTheme } from "./styles/themes/default";
 import { Page01 } from "./components/page01";
 import { Page02 } from "./components/page02";
 import { Page03 } from "./components/page03";
+import { NavBar } from "./components/NavBar";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <NavBar />
       <Page01 />
       <Page02 />
       <Page03 />
