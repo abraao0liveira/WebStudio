4f32c76a094f248e67089c652fb9b67f9bf0ea5a
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-16 21:51:47 -0300

    create style global and theme default

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: ${(props) => props.theme['green-900']};
+    color: ${(props) => props.theme['white']};
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+  html {
+    scroll-behavior: smooth;
+  }
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme['scroll-100']};
+    border-radius: 0.25rem;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: ${(props) => props.theme['scroll-300']};
+  }
+`
---
+++ src/styles/themes/default.js
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+  
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'scroll-100': '#888',
+  'scroll-300': '#555',
+
+  'green-900': '#12374a',
+  'green-800': '#124d5e',
+  'green-600': '#12707a',
+  'green-400': '#179e9d',
+  'green-200': '#20daca',
+}
