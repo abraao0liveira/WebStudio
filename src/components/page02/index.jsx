6dd8763fc7744b85a110a82700d7f5974cea4b08
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-16 21:52:06 -0300

    create page02

---
+++ src/components/page02/index.jsx
@@ -0,0 +1,9 @@
+import { Page02Container } from "./styles"
+
+export function Page02() {
+  return (
+    <Page02Container>
+      <h1>Page 02</h1>
+    </Page02Container>
+  )
+}
---
+++ src/components/page02/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Page02Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: ${(props) => props.theme['green-900']};
+  color: ${(props) => props.theme['white']};
+`
