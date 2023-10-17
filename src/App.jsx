4422b98c8b1b8423a115e15a01f4f55a99744426
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-16 21:51:03 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { Page01 } from "./components/page01";
+import { Page02 } from "./components/page02";
+import { Page03 } from "./components/page03";
+
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Page01 />
+      <Page02 />
+      <Page03 />
+
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
