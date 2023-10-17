a488c46e326bb42043b61e170f427f49c282c47b
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-16 22:01:58 -0300

    update page01(index.jsx and styles.js)

--- src/components/page01/styles.js
+++ src/components/page01/styles.js
@@ -4,15 +4,9 @@ export const Page01Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 90vh;
   background: ${(props) => props.theme['green-900']};
   color: ${(props) => props.theme['white']};
-
-  .navBar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
 
   .page01 {
     display: flex;
