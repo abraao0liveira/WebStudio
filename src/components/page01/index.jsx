3e9ca96de2ef8c6b19e784a1d60435c56c450b91
kayo duarte cavalcante <135269175+kayocavalcantte@users.noreply.github.com>
2023-10-18 18:18:52 -0300

    update page01
    index.jsx

--- src/components/page01/index.jsx
+++ src/components/page01/index.jsx
@@ -1,11 +1,36 @@
 import { Page01Container } from "./styles"
+import v from "../../assets/imgg.svg"
+import video from "../../assets/video.gif"
+
+
 
 export function Page01() {
   return (
     <Page01Container>
       <div className="page01">
-        <h1>Hello</h1>
+        <h1><strong>Web</strong>Studio</h1>
+        <h2>O melhor lugar para desenvolver o site da sua empresa!</h2>
       </div>
+      
+      <section>
+      <div className="image">
+        <img src={video}alt="" />
+        </div>
+        <div id="banner" >
+          <div className="BannerInspiracao">
+            <h1>Presença Online!</h1>
+            <p>Um site permite uma maior presença Online</p>
+          </div>
+          <div className="BannerInspiracao">
+            <h1>Alcance Global!</h1>
+            <p>Com estratégias de Marketing digital o seu site atrairá pessoas de qualquer parte do mundo</p>
+          </div>
+          <div className="BannerInspiracao">
+            <h1>Profissionalismo!</h1>
+            <p>Ter um site bem projetado e funcional transmite uma imagem de credibilidade e profissionalismo para os clientes</p>
+          </div>
+        </div>
+      </section>
     </Page01Container>
   )
 }
