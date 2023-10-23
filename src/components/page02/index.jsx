799eefa70d036a376e88afd7f6a8a64ad00a203b
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-23 16:19:55 -0300

    update page02 (index.jsx)

--- src/components/page02/index.jsx
+++ src/components/page02/index.jsx
@@ -4,7 +4,29 @@ export function Page02() {
   return (
     <Page02Container>
       <section id="models">
-        <h1>Page 02</h1>
+        <h1></h1>
+        <h3><strong></strong></h3>
+
+        <div className="boxes">
+          <div className="box">
+            <img src="" alt="" />
+            <h4></h4>
+            <p></p>
+          </div>
+
+          <div className="box">
+            <img src="" alt="" />
+            <h4></h4>
+            <p></p>
+          </div>
+          
+          <div className="box">
+            <img src="" alt="" />
+            <h4></h4>
+            <p></p>
+          </div>
+        </div>
+
       </section>
     </Page02Container>
   )
