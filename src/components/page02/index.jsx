06b449d7a43b915bc0a14232d3910b25dee3ebe3
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-23 16:11:44 -0300

    update page02 (index.jsx and styles.js)

--- src/components/page02/index.jsx
+++ src/components/page02/index.jsx
@@ -3,7 +3,9 @@ import { Page02Container } from "./styles"
 export function Page02() {
   return (
     <Page02Container>
-      <h1>Page 02</h1>
+      <section id="models">
+        <h1>Page 02</h1>
+      </section>
     </Page02Container>
   )
 }
