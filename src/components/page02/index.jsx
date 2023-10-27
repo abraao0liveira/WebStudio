0dc65faebbbb2e1e78aab52914dabbd37a7b7bb2
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-10-27 02:23:00 -0300

    update page02 (index.jsx and styles.js)

--- src/components/page02/index.jsx
+++ src/components/page02/index.jsx
@@ -1,38 +1,34 @@
 import { Page02Container } from "./styles"
-import { Atom } from "phosphor-react"
 
 export function Page02() {
   return (
     <Page02Container>
       <section id="models">
         <h1>Modelos</h1>
-        <h3><strong>A presença</strong> online de um negócio é substancialmente aprimorada por meio de um site, que fornece uma base sólida para o sucesso de sua empreitada na web.</h3>
+        <h3><strong>A presença</strong> digital de uma empresa é significativamente fortalecida por meio de um website, que estabelece uma base sólida para o êxito de sua presença online.</h3>
 
         <div className="boxes">
           <div className="box">
-            <Atom size={32} />
-            <h4>Página Única</h4>
-            <p>R$980,00</p>
+            <h2>Página Única</h2>
+            <h4>R$980,00</h4>
             <p>Um site com página única, ideal para promover um produto ou eventos.</p>
           </div>
 
           <div className="box">
-            <Atom size={32} />
-            <h4>Site de négocios</h4>
-            <p>R$1360,00</p>
+            <h2>Site de Négocios</h2>
+            <h4>R$1360,00</h4>
             <p>Um site com 3 páginas, ideal para promover sua empresa e produtos.</p>
-            <p>Página extra + R$299,90</p>
-            <p>Suporte + 199,90/mês</p>
+            <p>Página extra <strong>+ R$299,90</strong></p>
+            <p>Suporte <strong>+ 199,90/mês</strong></p>
           </div>
           
           <div className="box">
-            <Atom size={32} />
-            <h4>Site NuvemShop</h4>
-            <p>R$2830,00</p>
+            <h2>Site NuvemShop</h2>
+            <h4>R$2830,00</h4>
             <p>Um site criado por meio da plataforma NuvemShop, ideal para vendas.</p>
             <p>+ 3 produtos adicionados.</p>
             <p>+ Mentoria ensinando utilizar a plataforma.</p>
-            <p>Suporte + 199,90/mês</p>
+            <p>Suporte <strong>+ 199,90/mês</strong></p>
           </div>
         </div>
 
--- src/components/page02/styles.js
+++ src/components/page02/styles.js
@@ -3,9 +3,68 @@ import star1 from "../../assets/star1.png"
 
 export const Page02Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
   height: 100vh;
   background-image: url(${star1});
   color: ${(props) => props.theme['white']};
+
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  h3 {
+    width: 65%;
+    font-size: 1.2rem;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    strong {
+      color: ${(props) => props.theme['green-600']};
+    }
+  }
+
+  .boxes {
+    width: 75%;
+    height: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient( to bottom, 
+      ${(props) => props.theme['green-600']},
+      ${(props) => props.theme['green-800']}, 
+      ${(props) => props.theme['green-900']});
+    padding: 1.5rem;
+    margin-top: 3.5rem;
+    margin-left: auto;  
+    margin-right: auto;
+    border-radius: 15px;
+
+    .box {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 0 1rem;
+
+      h2 {
+        font-size: 1.18rem;
+        margin-bottom: 0.5rem;
+      }
+      h4 {
+        font-size: 0.95rem;
+        text-align: center;
+        margin-bottom: 0.75rem;
+      }
+      p {
+        font-size: 0.95rem;
+        text-align: center;
+        margin-bottom: 0.25rem;
+      }
+    }
+  }
 `
