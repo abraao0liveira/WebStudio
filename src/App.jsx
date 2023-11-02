52cc2248bc5fda232584d070660696ef77213bc6
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-11-02 14:25:05 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Page01 } from "./components/page01";
 import { Page02 } from "./components/page02";
 import { Page03 } from "./components/page03";
 import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
@@ -13,6 +14,7 @@ export function App() {
       <Page01 />
       <Page02 />
       <Page03 />
+      <Footer />
 
       <GlobalStyles />
     </ThemeProvider>
