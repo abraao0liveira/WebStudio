0f0ed248502d27a9bd1dcd7122cab56af7f6c7fd
kayo duarte cavalcante <135269175+kayocavalcantte@users.noreply.github.com>
2023-11-03 19:37:10 -0300

    update Footer [index.jsx and styles.js]

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,21 +5,52 @@ export function Footer() {
   return (
     <FooterContainer>
       <div className="box">
-        <div className="logo">
-          <Intersect size={38} />
-          <h1><strong>Web</strong>Studio</h1>
+        
+        <div className="global">
+          <div className="logo">
+            <Intersect size={38} />
+            <h1><strong>Web</strong>Studio</h1> 
+          </div>
+  
+          <div className="paragrafo">
+            <p>O melhor lugar para desenvolver o site da sua empresa!</p>
+          </div>
+
+          <div className="socialMedia">
+            <h2>Siga-nos</h2>
+            <a href="/"><InstagramLogo size={32} /></a>
+            <a href="/"><WhatsappLogo size={32} /></a>
+          </div>
         </div>
-        <p>O melhor lugar para desenvolver o site da sua empresa!</p>
+
+        <div className="form">
+          <h1>Fale Conosco</h1>
+          <form action="https://formsubmit.co/abraaooliveira.br@gmail.com" method="POST" className="form">
+            <label htmlFor="name">Nome</label>
+            <input type="text" name="name" placeholder="Nome" id="name" required/>
+
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Email" id="email" required/>
+
+            <label htmlFor="message">Mensagem</label>
+            <textarea name="message" placeholder="Mensagem" id="message" required></textarea>
+
+            <input type="hidden" name="_captcha" value="false"/>
+            <button type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
       </div>
 
-      <div className="socialMedia">
+      {/* <div className="socialMedia">
         <div className="instagram">
           <a href="/"><InstagramLogo size={32} />Instagram</a>
         </div>
         <div className="whatsApp">
           <a href="/"><WhatsappLogo size={32} />WhatsApp</a>
         </div>
-      </div>
+      </div> */}
     </FooterContainer>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
-  display: flex;
+  /* display: flex; */
   align-items: center;
-  height: 20vh;
+  height: 50vh;
   background-color: ${(props) => props.theme['gray-800']};
   color: ${(props) => props.theme['white']};
+
+  .box{
+    display: flex;
+    justify-content: space-evenly;
+    padding: 20px;
+
+    .logo{
+      display: flex;
+      margin-bottom: 10px;
+    }
+
+    .socialMedia{
+      margin-top: 30px;
+      
+      h2{
+        margin-bottom: 10px;
+      }
+      a{
+      text-decoration: none;
+      color: ${(props) => props.theme['white']};
+      margin: 5px;
+      
+      }
+    }
+
+    
+  }
+
 `
