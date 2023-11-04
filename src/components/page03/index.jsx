ff8af2d1415edbd8d68d24b757c04d0bbc39ad4d
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-11-04 01:22:02 -0300

    update page03 (index.jsx and styles.js)

--- src/components/page03/index.jsx
+++ src/components/page03/index.jsx
@@ -3,31 +3,8 @@ import { Page03Container } from "./styles"
 export function Page03() {
   return (
     <Page03Container>
-      <section id="contacts">
-        <div className="contact">
-          <h1>Entre em contato</h1>
-          <p>Nosso sucesso começa com o seu contato! Se você procura serviços de criação de sites excepcionais, estamos prontos para ouvir suas ideias e necessidades. Entre em contato conosco hoje e vamos transformar sua visão em realidade. Sua presença online começa aqui!</p>
-        </div>
-
-        <div className="form">
-          <h1>Fale Conosco</h1>
-          <form action="https://formsubmit.co/abraaooliveira.br@gmail.com" method="POST" className="form">
-            <label htmlFor="name">Nome</label>
-            <input type="text" name="name" placeholder="Nome" id="name" required/>
-
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="Email" id="email" required/>
-
-            <label htmlFor="message">Mensagem</label>
-            <textarea name="message" placeholder="Mensagem" id="message" required></textarea>
-
-            <input type="hidden" name="_captcha" value="false"/>
-            <button type="submit">
-              Enviar
-            </button>
-          </form>
-        </div>
-
+      <section id="projects">
+        <h1>Projetos</h1>
       </section>
     </Page03Container>
   )
--- src/components/page03/styles.js
+++ src/components/page03/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import star1 from "../../assets/star1.png"
 
 export const Page03Container = styled.div`
-  height: 80vh;
+  height: 90vh;
   background-image: url(${star1});
   color: ${(props) => props.theme['white']};
 `
