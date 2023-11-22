0546c723cc2c628a430f43d5c8ff343911d3c741
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-11-22 02:16:24 -0300

    update page03 (index.jsx and styles.js)

--- src/components/page03/index.jsx
+++ src/components/page03/index.jsx
@@ -4,7 +4,19 @@ export function Page03() {
   return (
     <Page03Container>
       <section id="projects">
-        <h1>Projetos</h1>
+        <h1>Projetos que já desenvolvemos.</h1>
+        <div className="sliderWrapper">
+          <div className="slider">
+            <img id="slide-1" src="https://images7.alphacoders.com/132/1329456.jpeg" alt="" />
+            <img id="slide-2" src="https://images4.alphacoders.com/132/1326330.png" alt="" />
+            <img id="slide-3" src="https://images.alphacoders.com/132/1326812.jpeg" alt="" />
+          </div>
+          <div className="sliderNav">
+            <a href="#slide-1"></a>
+            <a href="#slide-2"></a>
+            <a href="#slide-3"></a>
+          </div>
+        </div>
       </section>
     </Page03Container>
   )
--- src/components/page03/styles.js
+++ src/components/page03/styles.js
@@ -2,7 +2,57 @@ import styled from "styled-components";
 import star1 from "../../assets/star1.png"
 
 export const Page03Container = styled.div`
-  height: 90vh;
+  height: 100vh;
   background-image: url(${star1});
   color: ${(props) => props.theme['white']};
+
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  .sliderWrapper {
+    position: relative;
+    max-width: 52rem;
+    margin: 0 auto;
+  }
+  .slider {
+    display: flex;
+    aspect-ratio: 16/9;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+    box-shadow: 0 1.5rem 3rem -0.75rem hsla(0, 0%, 0%, 0.25);
+    border-radius: 0.5rem;
+  }
+  .slider::-webkit-scrollbar {
+    display: none;
+  }
+  .slider img {
+    flex: 1 0 100%;
+    scroll-snap-align: start;
+    object-fit: cover;
+  }
+  .sliderNav {
+    display: flex;
+    column-gap: 1rem;
+    position: absolute;
+    bottom: 1.25rem;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+  }
+  .sliderNav a {
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme['white']};
+    opacity: 0.75;
+    transition: opacity ease 250ms;
+  }
+  .sliderNav a:hover {
+    opacity: 1;
+  }
 `
