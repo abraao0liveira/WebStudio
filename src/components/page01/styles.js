e50bdafe6a4fb13d78ce937bf34c94c1e0bbc6e9
kayo duarte cavalcante <135269175+kayocavalcantte@users.noreply.github.com>
2023-12-26 15:49:57 -0300

    update pag01 ( styles.js)

--- src/components/page01/styles.js
+++ src/components/page01/styles.js
@@ -64,4 +64,47 @@ export const Page01Container = styled.div`
       color: ${(props) => props.theme['white']};
     }
   }
+
+  @media screen and (max-width: 734px){
+    height: auto;
+    .page01 {
+      display: none;
+    }
+    section {
+      flex-direction: column;
+      width: 100%;
+    }
+    .image {
+      width: 100%;
+      .movie {
+        width: 100%;
+      }
+    }
+    #banner {
+      width: 100%;
+      display: block;
+    }
+    .BannerInspiracao {
+      width: 80%;
+      margin: 10px;
+      padding: 12px;
+      margin: auto;
+      margin-top: 10px;
+      border-radius: 25px;
+      background-color: ${(props) => props.theme['gray-800']};
+      h1 {
+        font-size: 1.18rem;
+        display: flex;
+        margin: center;
+        align-items: center;
+        justify-content: center;
+        color: ${(props) => props.theme['white']};
+      }
+      p {
+        font-size: 0.95rem;
+        margin: 0.5rem 0rem 0.75rem 0rem;
+        color: ${(props) => props.theme['white']};
+      }
+    }
+  }
 `
